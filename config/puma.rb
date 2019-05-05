def3e70c897f896c91f590d8b6167704bb642b6a
Mike Rogers <me@mikerogers.io>
2019-05-05 12:26:54 +0100

    Adding whitelisting for ngrok host for rails 6

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+plugin :ngrok_tunnel if ENV['NGROK_TUNNEL_ENABLED']
